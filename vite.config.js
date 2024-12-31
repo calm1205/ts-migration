d2c9150365349519c6cc1253938c59d51b1d69a6
calm1205 <authentic.nagi@gmail.com>
2024-12-31 03:26:35 +0000

    feat: path alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { fileURLToPath } from "node:url"
+import path from "node:path"
 
+const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname, "src"),
+    },
+  },
 })
